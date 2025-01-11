6bbe35f07d63606c797db98d501f34a1568af648
nertonm <thiago2nerton@gmail.com>
2025-01-11 17:59:25 -0300

    Adding spring initializr files

---
+++ src/main/java/com/backendconnect/BackendconnectApplication.java
@@ -0,0 +1,13 @@
+package com.backendconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendconnect/BackendconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
