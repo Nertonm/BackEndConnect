6bf62562c2b8ebdfea8772f2c03c3b2db358dd22
nertonm <thiago2nerton@gmail.com>
2025-01-12 03:10:57 -0300

    hello world

--- src/main/java/com/backendconnect/BackendconnectApplication.java
+++ src/main/java/com/backendconnect/BackendconnectApplication.java
@@ -2,6 +2,15 @@ package com.backendconnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class BackendconnectApplication {
@@ -10,4 +19,31 @@ public class BackendconnectApplication {
 		SpringApplication.run(BackendconnectApplication.class, args);
 	}
 
+
+	@Configuration
+	@EnableWebSecurity
+	public class WebSecurityConfig {
+
+		@Bean
+		public UserDetailsService userDetailsService() {
+			UserDetails user = User.withDefaultPasswordEncoder()
+					.username("user")
+					.password("password")
+					.roles("USER")
+					.build();
+
+			return new InMemoryUserDetailsManager(user);
+		}
+
+		@Bean
+		public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+			http
+					.authorizeHttpRequests(authorize -> authorize
+							.anyRequest().permitAll() // Allow all requests without authentication
+					)
+					.csrf(csrf -> csrf.disable()); // Disable CSRF protection if not needed
+
+			return http.build();
+		}
+	}
 }
