aaf52b270358f747f2b3cca1868acb4b0e92c79e
nertonm <thiago2nerton@gmail.com>
2025-01-15 00:06:40 -0300

    Modificando a segurança para stateless

--- src/main/java/com/backendconnect/BackendconnectApplication.java
+++ src/main/java/com/backendconnect/BackendconnectApplication.java
@@ -19,31 +19,4 @@ public class BackendconnectApplication {
 		SpringApplication.run(BackendconnectApplication.class, args);
 	}
 
-
-	@Configuration
-	@EnableWebSecurity
-	public class WebSecurityConfig {
-
-		@Bean
-		public UserDetailsService userDetailsService() {
-			UserDetails user = User.withDefaultPasswordEncoder()
-					.username("user")
-					.password("password")
-					.roles("USER")
-					.build();
-
-			return new InMemoryUserDetailsManager(user);
-		}
-
-		@Bean
-		public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-			http
-					.authorizeHttpRequests(authorize -> authorize
-							.anyRequest().permitAll() // Allow all requests without authentication
-					)
-					.csrf(csrf -> csrf.disable()); // Disable CSRF protection if not needed
-
-			return http.build();
-		}
-	}
 }
