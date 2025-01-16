96a9f81ac928cbecfe0ba05367ad761d08959ac4
nertonm <thiago2nerton@gmail.com>
2025-01-16 13:40:22 -0300

    making Delete logic

--- src/main/java/com/backendconnect/domain/topic/TopicRepository.java
+++ src/main/java/com/backendconnect/domain/topic/TopicRepository.java
@@ -10,8 +10,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
-    @Query("SELECT t FROM Topic t WHERE t.course = :courseId AND YEAR(t.createdAt) = :year ORDER BY t.createdAt DESC")
+    @Query("SELECT t FROM Topic t WHERE t.course = :courseId AND YEAR(t.createdAt) = :year  AND t.status = true ORDER BY t.createdAt DESC")
     List<Topic> findByCourseAndYear(long courseId, int year, Pageable pageable);
+
     Optional<Topic> findByTitleAndMessage(String title, String message);
+
+    @Query("SELECT t FROM Topic t WHERE t.status = true AND t.id = :id")
+    Optional<Topic> getRefenceById(long id);
+
+    @Query("SELECT t FROM Topic t WHERE t.status = true ORDER BY t.id DESC")
     Page<Topic> findAllByOrderByIdDesc(Pageable pageable);
 }
